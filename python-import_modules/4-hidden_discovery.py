50b5ce886799fd29b5bff9cd5f714b04ca710772
mwaniasam <s.mwania@alustudent.com>
2024-06-19 22:21:29 -0700

    debugged 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
+import hidden_4
 
+if _name_ == "_main_":
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    sorted_names = sorted(name for name in names if not name.startswith(""))
+    for name in sorted_names:
+        print(name)
