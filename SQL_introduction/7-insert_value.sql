df62f259491967b7f61ea1bd113e8e3d9cfbaabc
mwaniasam <s.mwania@alustudent.com>
2024-07-18 05:31:20 -0700

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,6 @@
---This inserts into
+--This inserts into and verifies
 
-INSERT INTO ?? (id, name) VALUES (89, 'Best School');
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+
+SELECT * FROM first_table WHERE id = 89;
+
