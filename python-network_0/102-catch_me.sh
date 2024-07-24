7316a5b8fb3203692bf0650f3138d870e11193a0
mwaniasam <s.mwania@alustudent.com>
2024-07-24 12:05:49 -0700

    102-catch_me.sh

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script to send a PUT request to 0.0.0.0:5000/catch_me to receive a response containing "You got me!" in the body
-
-curl -s -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" http://0.0.0.0:5000/catch_me
-
+# Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:HolbertonSchool" -d "user_id=98"
