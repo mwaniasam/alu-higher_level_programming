5b10a04cd4aecb7220b2bcefac2ec39935f7b989
mwaniasam <s.mwania@alustudent.com>
2024-07-25 07:28:10 -0700

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Python script that fetches."""
-
+"""
+ fetches https://alx-intranet.hbtn.io/status
+"""
 
 import requests
-if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
+
+if __name__ == '__main__':
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
