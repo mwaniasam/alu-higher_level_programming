36dbb60ef84a1375df30aade57d136b7ebbd6d62
mwaniasam <s.mwania@alustudent.com>
2024-08-01 11:12:44 -0700

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all
-values in the states table
-of hbtn_0e_0_usa where name matches the argument
-"""
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
-def my_safe_filter_states():
-    """ takes in an argument and displays all
-    values in the states table
-    of hbtn_0e_0_usa where name matches the argument
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3]
-                         )
-    cursor = db.cursor()
-
-    cursor \
-        .execute("SELECT * FROM states WHERE name= %s\
-                ORDER BY id ASC", (sys.argv[4],))
-
-    records = cursor.fetchall()
-    for data in records:
-        print(data)
-
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    my_safe_filter_states()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
