ec62ae49c55a1d2b577a284a3141c2d36504edd3
mwaniasam <s.mwania@alustudent.com>
2024-08-01 11:56:52 -0700

    10-model_state_my_get.py

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
-"""Print State obj with 'name' passed as arg from db 'hbtn_0e_6_usa'
 """
-import sys
-from sqlalchemy.orm import sessionmaker
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
-from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    res = session.query(State.id).filter(State.name == sys.argv[4])
-
-    if (res.first() is None):
-        print("Not found")
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}'.format(state.id))
     else:
-        print(res[0][0])
+        print("Not found")
