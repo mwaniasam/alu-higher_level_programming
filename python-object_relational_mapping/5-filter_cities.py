efac6f29358d81eb3b94e0a6786272c032ecbdfb
mwaniasam <s.mwania@alustudent.com>
2024-08-01 11:09:19 -0700

    5-filter_cities.py

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,38 @@
 #!/usr/bin/python3
+""" lists all cities from the database hbtn_0e_4_usa
 """
-Script that takes in name of state as argument and list
-"""
+
+import MySQLdb
+import sys
+
+
+def my_safe_filter_states():
+    """ lists all cities from the database
+    hbtn_0e_4_usa"""
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3]
+                         )
+    cursor = db.cursor()
+
+    cursor \
+        .execute("SELECT cities.name FROM cities\
+                    JOIN states ON cities.state_id = states.id\
+                    AND states.name = %s\
+                    ORDER BY cities.id ASC", (sys.argv[4],))
+
+    records = cursor.fetchall()
+    cites = []
+    for data in records:
+        cites.append(data[0])
+
+    print(", ".join(cites))
+    cursor.close()
+    db.close()
 
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    check = (argv[4], )
-    cur.execute("SELECT * FROM cities JOIN states\
-    ON cities.state_id = states.id WHERE states.name = %s\
-    ORDER BY cities.id ASC", check)
-    lst = cur.fetchall()
-    cities = []
-    for r in lst:
-        if r[4] == check[0]:
-            cities.append(r[2])
-    print(', '.join(cities))
-    cur.close()
-    db.close()
+    my_safe_filter_states()
