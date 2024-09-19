3b3b8be9f5e4a44d45aa738c0a46db2b0482285f
mwaniasam <s.mwania@alueducation.com>
2024-09-19 07:17:32 +0000

    Semi standard validation

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 function factorial(n) {
   if (isNaN(n) || n === 0) {
     return 1;
@@ -7,7 +6,6 @@ function factorial(n) {
     return n * factorial(n - 1);
   }
 }
-
 const number = process.argv[2];
 const result = factorial(Number(number));
 console.log(result);
