817a528ec3d82d84ebaeebc860f20ba4c8e67af6
mwaniasam <s.mwania@alueducation.com>
2024-09-23 07:23:24 +0000

    101-sorted.js

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -3,12 +3,15 @@
 const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-  const value = dict[key];
-  if (newDict[value] === undefined) {
-    newDict[value] = [];
-    newDict[value].push(key);
-  } else {
-    newDict[value].push(key);
-  }
+    const value = dict[key];
+    console.log(`Processing key: ${key}, value: ${value}`);
+    if (newDict[value] === undefined) {
+        newDict[value] = [];
+        newDict[value].push(key);
+        console.log(`Creating new entry in newDict: ${value} => ${newDict[value]}`);
+    } else {
+        newDict[value].push(key);
+        console.log(`Adding ${key} to existing entry in newDict: ${value} => ${newDict[value]}`);
+    }
 }
 console.log(newDict);
