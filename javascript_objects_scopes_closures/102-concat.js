6d14b1ab1f999cf049e9632d0ecb81668388b058
mwaniasam <s.mwania@alueducation.com>
2024-09-23 08:21:17 +0000

    102-concat.js

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
 const fs = require('fs');
-const fileA = fs.readFileSync(args[0]);
-const fileB = fs.readFileSync(args[1]);
-const fileC = `${fileA}${fileB}`;
-fs.writeFileSync(args[2], fileC, (err) => {
-  if (err) {
-    console.log(err);
-  }
-});
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
