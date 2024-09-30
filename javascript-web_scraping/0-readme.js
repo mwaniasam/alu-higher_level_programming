df625c887cdea1f5ff5234b70e8009267210886a
mwaniasam <s.mwania@alueducation.com>
2024-09-30 06:18:58 +0000

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const crypto = require('crypto');
 
 fs.readFile(process.argv[2], (err, contents) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-
-    // Calculate the SHA-256 hash of the file contents
-    const hash = crypto.createHash('sha256').update(contents).digest('hex');
-
-    // Print the hash instead of the original contents
-    console.log(hash);
+  err ? console.log(err) : console.log(contents.toString());
 });
