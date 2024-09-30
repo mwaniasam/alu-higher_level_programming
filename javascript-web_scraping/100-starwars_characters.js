0bdf66cf2c3201c5d6888aa90771545200d2f03f
mwaniasam <s.mwania@alueducation.com>
2024-09-30 06:48:32 +0000

    100-starwars_characters.js

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -17,14 +17,28 @@ request(options, (error, response, body) => {
 
   const characters = body.characters;
 
-  characters.forEach(characterUrl => {
-    request(characterUrl, (characterError, characterResponse, characterBody) => {
-      if (characterError) {
-        console.error('Error fetching character data:', characterError);
-        return;
-      }
-
-      console.log(characterBody.name);
+  const characterNames = characters.map(characterUrl => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (characterError, characterResponse, characterBody) => {
+        if (characterError) {
+          reject(characterError);
+        } else {
+          resolve(characterBody.name);
+        }
+      });
     });
   });
+
+  Promise.all(characterNames)
+    .then(names => {
+      if (names.length !== characters.length) {
+        console.log('Missing some characters');
+        console.log(names);
+      } else {
+        console.log('OK');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching character data:', error);
+    });
 });
