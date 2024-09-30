89cb409f01ae7425f7f4c50ee55dad4420eec94f
mwaniasam <s.mwania@alueducation.com>
2024-09-30 06:33:30 +0000

    100-starwars_characters.js

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const options = {
+  url: `https://swapi.dev/api/films/${movieId}`,
+  json: true
+};
+
+request(options, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching movie data:', error);
+    return;
+  }
+
+  const characters = body.characters;
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, (characterError, characterResponse, characterBody) => {
+      if (characterError) {
+        console.error('Error fetching character data:', characterError);
+        return;
+      }
+
+      console.log(characterBody.name);
+    });
+  });
+});
