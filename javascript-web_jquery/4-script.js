a36417b94dba948a889a70d70325b9034240b3e9
mwaniasam <s.mwania@alueducation.com>
2024-10-02 17:29:32 +0000

    4-script.js

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+$('#red_header').click(function () {
+    if ($('header').attr('class') === 'red') {
+        $('header').attr('class', 'green');
+    } 
+    else if ($('header').attr('class') === 'green') {
+        $('header').attr('class', 'red');
+    }
+    else {
+        $('header').addClass('red');
+    }
+  });
